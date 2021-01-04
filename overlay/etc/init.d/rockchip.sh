8d5e7509ee390b64777c079e0813f096ae809c2f
Caesar Wang <wxt@rock-chips.com>
2021-01-04 09:42:46 +0800

    ubuntu: update ubuntu20.04
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay/etc/init.d/rockchip.sh
+++ overlay/etc/init.d/rockchip.sh
@@ -118,9 +118,15 @@ then
 fi
 
 # Create dummy video node for chromium V4L2 VDA/VEA with rkmpp plugin
+
+touch /dev/video-dec0
+touch /dev/video-enc0
+
+chmod 666 /dev/video-dec0
+chmod 666 /dev/video-enc0
+
 echo dec > /dev/video-dec0
 echo enc > /dev/video-enc0
-
 # The chromium using fixed pathes for libv4l2.so
 ln -rsf /usr/lib/*/libv4l2.so /usr/lib/
 [ -e /usr/lib/aarch64-linux-gnu/ ] && ln -Tsf lib /usr/lib64
